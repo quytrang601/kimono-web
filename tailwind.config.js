e93696f55f9e49aa5c5981eb120e46bcfca63a98
quytrang601 <quytrang601@gmail.com>
2026-04-02 21:50:11 +0700

    change font serif to Playfair Display

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                // Ghi đè class 'font-serif' cũ bằng font mới
+                serif: ['"Playfair Display"', 'serif'],
+
+                // Nếu bạn muốn đổi cả font mặc định (không chân) toàn web, thì thêm dòng sans này:
+                // sans: ['"Tên Font Mới"', 'sans-serif'], 
+            },
+        },
     },
     plugins: [],
 }
